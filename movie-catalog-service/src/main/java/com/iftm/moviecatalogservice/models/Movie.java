4c48b0835038ed76cc0963127479d907091125be
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-11-21 16:06:07 -0300

    Usando Bean para criar um RestTemplate

--- movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/models/Movie.java
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/models/Movie.java
@@ -1,32 +1,15 @@
 package com.iftm.moviecatalogservice.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 
 	private String movieId;;
 	private String name;
-
-	public Movie(String movieId, String name) {
-		this.movieId = movieId;
-		this.name = name;
-	}
-
-	public Movie() {
-	}
-
-	public String getMovieId() {
-		return movieId;
-	}
-
-	public void setMovieId(String movieId) {
-		this.movieId = movieId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 }
--- movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +18,16 @@ import com.iftm.moviecatalogservice.models.Rating;
 @RestController
 @RequestMapping("/catalog")
 public class CatalogResource {
+	
+	@Autowired
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		RestTemplate restTemplate = new RestTemplate();
 
 		List<Rating> ratings = Arrays.asList(new Rating("12", 4), new Rating("15", 7));
+		
+		//UserRating ratings = restTemplate.getForObject("hhtp://localhost:8083/raingsdata/users/" + userId, UserRating.class);
 
 		return ratings.stream().map(rating -> {
 			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
