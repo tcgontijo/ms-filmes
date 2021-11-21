d17db2454faf67d788502ba57ec2f0411551615e
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-11-21 15:46:58 -0300

    RestTemplate chamando microsservice API movie

---
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/models/Rating.java
@@ -0,0 +1,14 @@
+package com.iftm.moviecatalogservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rating {
+	private String movieId;
+	private Integer rating;
+
+}
--- movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
@@ -1,14 +1,18 @@
 package com.iftm.moviecatalogservice.resources;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.iftm.moviecatalogservice.models.CatalogItem;
+import com.iftm.moviecatalogservice.models.Movie;
+import com.iftm.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -16,7 +20,15 @@ public class CatalogResource {
 
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		return Collections.singletonList(new CatalogItem("Transformers", "Filme de Robô", 4));
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<Rating> ratings = Arrays.asList(new Rating("12", 4), new Rating("15", 7));
+
+		return ratings.stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Filmao", rating.getRating());
+		}).collect(Collectors.toList());
+
 	}
 
 }
